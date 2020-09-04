cde63f31213cc21e2059da75807d142ea0498f4d
Kalyani <kalyanikolhe2001@gmail.com>
2020-09-04 10:15:21 +0530

    first commit

---
+++ Basic_Operation.java
@@ -0,0 +1,12 @@
+
+public class Basic_Operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 20;
+		int y = 5;
+		int z = x + y;
+		System.out.println(z);
+	}
+
+}
